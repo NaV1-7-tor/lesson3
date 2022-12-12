8d5981eff583b9873453658c050e6d2264f5319c
Ivan <makovkanavi@gmail.com>
2022-12-12 17:09:14 +0200

    Initial commit

---
+++ klumba.py
@@ -0,0 +1,4 @@
+shirina = int(input("Ширина ограды:"))
+dolgota = int(input("Долгота ограды:"))
+print("Нужно ", shirina*2+dolgota*2, " метров забора")
+print("Можно посадить", shirina*dolgota, "сажанцев")
